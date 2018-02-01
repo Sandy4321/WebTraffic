1d2f9d557f6a849eff67d1b2e58b0ae19436930a
pedro lopes <pedroallenrevez@gmail.com>
2018-02-01 18:12:54 +0000

    preprocessing series with multiple features and batching

--- torch_loader.py
+++ torch_loader.py
@@ -5,6 +5,7 @@ import numpy as np
 
 import torch
 from torch.utils.data import Dataset
+from torch.autograd import Variable
 
 import preprocessing as pre
 
@@ -13,7 +14,7 @@ class WebTrafficDataset(Dataset):
     """WebTraffic dataset."""
 
     def __init__(self, root_dir, file_base, threshold=0.0,
-            forecast_days=60, sliding_window=30, start=None, end=None, transform=None):
+            forecast_days=60, lags=30, start=None, end=None, transform=None):
         """
         Args:
             file_base (string): Basename of the file with annotations.
@@ -23,7 +24,7 @@ class WebTrafficDataset(Dataset):
         """
         self.root_dir = root_dir
         self.file_base = file_base
-        self.sliding_window = sliding_window
+        self.lags = lags
 
         # load the dataset
         PKL_PATH = os.path.join(root_dir, file_base + '.pkl')
@@ -97,15 +98,40 @@ class WebTrafficDataset(Dataset):
 
         print('Done loading and preprocessing set!')
 
+    def batch_series(self, data:np.ndarray, lags=1, drop_nan=True):
+        cols = []
+        for i in range(len(data)):
+            tmp = pre.shift(data, lags-i)
+            # extract number of lags
+            cols += [tmp[:lags]]
+        series = np.array(cols)
+        target = np.array([data[i][0] for i in range(data.shape[0])])
+
+        # drop batches that have a nan value in the series position
+        if drop_nan:
+            size = series.shape[0]
+            # build mask for first sliding window nan values
+            mask = np.full(size, True, dtype='bool')
+            for i in range(size):
+                if np.isnan(series[i][0][0]):
+                    mask[i] = False
+                else:
+                    break
+            series = series[mask]
+            target = target[mask]
+
+        return series, target
+
+    def get_series(self, idx):
+        return self.df.iloc[idx].values
+
     def __len__(self):
         return self.df.shape[0]
 
     def __getitem__(self, idx):
         tseries = self.df.iloc[idx].values
-        # turning timeseries into batches
-        #tseries_batch = pre.series_supervised_np(tseries, lags=self.sliding_window, drop_nan=True)
-
         agent = self.agents[idx]
+        access = self.access[idx]
         country = self.countries[idx]
         site = self.site[idx]
         median = self.page_median.iloc[idx]
@@ -113,18 +139,35 @@ class WebTrafficDataset(Dataset):
         yearly = self.yearly[idx]
         dow = self.day_of_week
 
+        # Assemble features into a stacked vector [len(tseries) x #features]
+        x_dow = dow[:len(tseries)]
+        y_dow = dow[len(tseries):]
+        
+        stack = np.stack((yearly, quarterly, median))
+        concat = np.concatenate((agent, site, country, stack))
+        
+        result = np.tile(concat, [len(tseries), 1]).transpose()
+        series = np.vstack((tseries, x_dow, result)).transpose()
+
+        # Batch stacked vector into the number of required lags
+        datapoint, target = self.batch_series(series, lags=self.lags)
+        datapoint = Variable(torch.from_numpy(datapoint))
+        target = Variable(torch.from_numpy(target))
+
         # we need to concat every feature into a single tensor
-        datapoint = {
-                'tseries': tseries,
-                'agent': agent,
-                'country': country,
-                'site': site,
-                'median': median,
-                'quarterly': quarterly,
-                'yearly': yearly,
-                'dow': dow,
-                }
-        return datapoint
+        #datapoint = {
+        #        'tseries': tseries,
+        #        'agent': agent,
+        #        'country': country,
+        #        'site': site,
+        #        'access': access,
+        #        'median': median,
+        #        'quarterly': quarterly,
+        #        'yearly': yearly,
+        #        'dow': dow,
+        #        }
+        return datapoint, target
+
 
 
 if __name__ == "__main__":
@@ -133,11 +176,11 @@ if __name__ == "__main__":
     parser.add_argument('file_base')
     parser.add_argument('--threshold', default=0.0, type=float, help="Series minimal length threshold (pct of data length)")
     parser.add_argument('--forecast_days', default=60, type=int, help="Add N days in a future for prediction")
-    parser.add_argument('--sliding_window', default=30, type=int, help="N days for the sliding window.")
+    parser.add_argument('--lags', default=30, type=int, help="N days for the sliding window.")
     parser.add_argument('--start', help="Effective start date. Data before the start is dropped")
     parser.add_argument('--end', help="Effective end date. Data past the end is dropped")
     args = parser.parse_args()
 
-    ds = WebTrafficDataset(args.data_dir, args.file_base, args.threshold, args.forecast_days, args.sliding_window,
+    ds = WebTrafficDataset(args.data_dir, args.file_base, args.threshold, args.forecast_days, args.lags,
         args.start, args.end)
-    print(ds[0])
+    torch.save(ds, 'data/dataset.pt')
